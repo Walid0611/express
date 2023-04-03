3d1f82261af6733a4d0cdd0bea90541f91fe224f
walid meftah <walid.meftah97@gmail.com>
2023-04-03 02:09:42 +0100

    seconde change

--- midelware/date.js
+++ midelware/date.js
@@ -2,7 +2,7 @@ const date=(req,res,next)=>{
 let date = new Date();
 let h =date.getHours()
 
-if( !(h >= 9 &&  h <= 11)    ){
+if( !(h >= 9 &&  h <= 17)    ){
 res.send('you are not welcome ')
 }
 next()
